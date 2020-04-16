075936c27883f7eedb656ffbe893bdac208db1c4
lockdownpt <guerra70@live.com.pt>
2020-04-16 12:02:02 +0100

    compile is working

--- compile.sh
+++ compile.sh
@@ -6,7 +6,8 @@
 # Compiled code is placed under ./build/
 # Modify it if needed to suite your purpose
 
-javac -d build */*.java
+javac -d build */*/*.java */*.java
+
 
 # If you are using jar files, and these must be in some particular
 #  place under the build tree, you should copy/move those jar files.
